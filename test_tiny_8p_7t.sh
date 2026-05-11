94efb2ba906614c2cc76d856165e18725862679d
CCmii <liuchang23@nudt.edu.cn>
2026-05-11 14:52:15 +0800

    添加其他测试脚本

---
+++ test_tiny_8p_7t.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -J Heme_tiny_8p_7t
+#SBATCH -p iris
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH -t 1:00:00
+#SBATCH -o log_%j.out
+#SBATCH --exclusive
+
+source /global/exafs/users/rdmaworkshop10/lc/HemeLB/optimized/HemePure/env.sh
+
+BASE=/global/exafs/users/rdmaworkshop10/lc/HemeLB
+EXE=/global/exafs/users/rdmaworkshop10/lc/HemeLB/optimized/HemePure/src/build_PP_Benchmark/hemepure
+INPUT=$BASE/Bifurcation-TINY/input_PP.xml
+OUT=/global/exafs/users/rdmaworkshop10/lc/HemeLB/optimized/result/test_tiny_8p_7t
+
+rm -rf $OUT
+
+export OMP_NUM_THREADS=7
+export OMP_PROC_BIND=close
+export KMP_AFFINITY=compact,1,0
+export KMP_BLOCKTIME=0
+
+mpirun -np 8 \
+  -genv I_MPI_PIN_DOMAIN=numa \
+  -genv I_MPI_PIN_ORDER=compact \
+  -genv OMP_NUM_THREADS=7 \
+  $EXE -in $INPUT -out $OUT
